3b9588c3c30a965e0774a1b77fd8613a279a513d
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-31 11:41:40 -0500

    Copying code from repository CPP-Functions

---
+++ Returning_Values.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+//declaring functions
+void deployMenu();	
+void processSelection(); 
+
+
+//MAIN FUNCTION
+int main(){
+	deployMenu();
+	processSelection();
+	
+	//Flushing the buffer
+	int c;
+	do {
+		c=cin.get();
+	}while(c!='\n'&&c!=EOF);
+	
+
+
+	//waiting for the user to press Enter
+	cin.get();
+	return 0;
+}
+
+void deployMenu(){
+	cout<<"1. Save Information"<<endl;
+	cout<<"2, Delete Information"<<endl;
+	cout<<"3. Edit Information"<<endl;
+	cout<<"4. QUIT"<<endl;
+}
+
+void processSelection(){
+	int option=0;
+	do{
+		cout<<"Press a number from 1 to 4"<<endl;
+		cin>>option;
+	}while(option!=1&&option!=2&&option!=3&&option!=4);
+	
+	cout<<"Processing..."<<endl;
+	sleep(3);
+	switch(option){
+		case 1:
+			cout<<"Information Saved"<<endl;
+			break;
+		case 2:
+			cout<<"Information Deleted"<<endl;
+			break;
+		case 3:
+
